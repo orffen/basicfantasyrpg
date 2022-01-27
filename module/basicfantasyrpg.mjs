fe0027bee63569afa831f3d6b874a59d697df165
Steve Simenic <orffen@orffenspace.com>
2022-01-27 19:59:07 +1000

    Clean up scss (not used)

--- module/basicfantasyrpg.mjs
+++ module/basicfantasyrpg.mjs
@@ -30,8 +30,8 @@ Hooks.once('init', async function() {
    * @type {String}
    */
   CONFIG.Combat.initiative = {
-    formula: "1d20 + @abilities.dex.mod",
-    decimals: 2
+    formula: "1d6 + @abilities.dex.mod",
+    decimals: 0
   };
 
   // Define custom Document classes
