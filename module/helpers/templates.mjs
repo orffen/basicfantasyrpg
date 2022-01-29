9df13d4af899253babcc15fee2642ac1b476047b
Steve Simenic <orffen@orffenspace.com>
2022-01-30 09:41:01 +1000

    Tweak character sheet

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -8,6 +8,7 @@
 
     // Actor partials.
     "systems/basicfantasyrpg/templates/actor/parts/actor-combat.html",
+    "systems/basicfantasyrpg/templates/actor/parts/actor-description.html",
     "systems/basicfantasyrpg/templates/actor/parts/actor-items.html",
     "systems/basicfantasyrpg/templates/actor/parts/actor-spells.html",
     "systems/basicfantasyrpg/templates/actor/parts/actor-effects.html",
