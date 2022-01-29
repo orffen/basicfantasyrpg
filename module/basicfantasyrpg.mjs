8b085e9e5f8801ea36a16968f80d3d5a1571afd3
Steve Simenic <orffen@orffenspace.com>
2022-01-29 23:03:36 +1000

    Fix initiative formula

--- module/basicfantasyrpg.mjs
+++ module/basicfantasyrpg.mjs
@@ -30,7 +30,7 @@ Hooks.once('init', async function() {
    * @type {String}
    */
   CONFIG.Combat.initiative = {
-    formula: "1d6 + @abilities.dex.bonus + @initBonus.value",
+    formula: "max(1, 1d6 + @abilities.dex.bonus + @initBonus.value)",
     decimals: 0
   };
 
