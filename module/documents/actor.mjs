28b1106fe409e5aa9ca01a991d1f2d1849138a34
Steve Simenic <orffen@orffenspace.com>
2022-01-29 16:27:42 +1000

    Update actor.mjs

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -50,7 +50,7 @@ export class BasicFantasyRPGActor extends Actor {
 
     // Loop through ability scores, and add their modifiers to our sheet output.
     for (let [key, ability] of Object.entries(data.abilities)) {
-      // Calculate the modifier using d20 rules.
+      // Calculate the ability modifier
       ability.mod = this._calculateAbilityModifier(ability.value);
     }
   }
