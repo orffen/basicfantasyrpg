e814e0f87f5b6ee823aaee3e1e9824ad1eeb77c9
Steve Simenic <orffen@orffenspace.com>
2022-02-01 13:27:20 +1000

    Character sheet localization pass
    - also renamed effects to features; needs more work
    - spells tab still not localized

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -11,6 +11,6 @@
     "systems/basicfantasyrpg/templates/actor/parts/actor-description.html",
     "systems/basicfantasyrpg/templates/actor/parts/actor-items.html",
     "systems/basicfantasyrpg/templates/actor/parts/actor-spells.html",
-    "systems/basicfantasyrpg/templates/actor/parts/actor-effects.html",
+    "systems/basicfantasyrpg/templates/actor/parts/actor-features.html",
   ]);
 };
