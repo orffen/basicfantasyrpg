1d0b0d1be04772aaaa52efb9972f1aec8f528794
Orffen <orffen@orffenspace.com>
2023-07-24 08:45:47 +1000

    Remove unused handlebars helper

--- module/basicfantasyrpg.mjs
+++ module/basicfantasyrpg.mjs
@@ -52,17 +52,6 @@ Hooks.once('init', async function() {
 /*  Handlebars Helpers                          */
 /* -------------------------------------------- */
 
-// If you need to add Handlebars helpers, here are a few useful examples:
-Handlebars.registerHelper('concat', function() {
-  var outStr = '';
-  for (var arg in arguments) {
-    if (typeof arguments[arg] != 'object') {
-      outStr += arguments[arg];
-    }
-  }
-  return outStr;
-});
-
 Handlebars.registerHelper('toLowerCase', function(str) {
   return str.toLowerCase();
 });
