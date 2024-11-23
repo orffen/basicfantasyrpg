06dcdeb68559c9249b79ba16d99f3f7fc0a78d6c
DC23 <jugglindan@gmail.com>
2024-11-23 15:33:22 +1100

    Added the remaining system setting scaffolding. Added a setting for auto rolling token HP. #18 Fixes #75

--- module/settings/settings.mjs
+++ module/settings/settings.mjs
@@ -1,2 +1,26 @@
+/**
+ * pseudo-enum to make setting ID references less error prone
+ */
+export const SETTINGS = {
+  AUTO_ROLL_TOKEN_HP: 'autoRollTokenHP'
+}
+
+// Use this internally for now. Refactoring the whole system is too big a job!
+const SYSTEM_ID = 'basicfantasyrpg'
+
 export function registerSettings () {
+  /**
+   * If we have any settings submenus, they'd be defined in separate files and called from here.
+   */
+
+  game.settings.register(SYSTEM_ID, SETTINGS.AUTO_ROLL_TOKEN_HP, {
+    name: 'BASICFANTASYRPG.AutoRollTokenHP.name',
+    hint: 'BASICFANTASYRPG.AutoRollTokenHP.hint',
+    scope: 'world',
+    config: true,
+    type: Boolean,
+    default: true,
+    requiresReload: false,
+    restricted: true,  // GM-only setting
+  })
 }
